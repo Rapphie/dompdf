462eca72cac9a9996b7bb7ee4e753eb349829bdb
Rapphie <181870325+Rapphie@users.noreply.github.com>
2026-02-17 01:45:35 +0800

    - modified home view to include sre - updated web.php route

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,13 @@
 <?php
 
-use App\Http\Controllers\FinancialReportController;
+use App\Http\Controllers\AMDFinancialReportController;
+use App\Http\Controllers\StatementOfReceiptsAndExpidenturesController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', [FinancialReportController::class, 'index'])->name('reports.home');
-Route::get('/reports/download', [FinancialReportController::class, 'download'])->name('reports.download');
+Route::get('/', fn () => view('home'))->name('home');
+
+Route::get('/reports/preview', [AMDFinancialReportController::class, 'preview'])->name('reports.preview');
+Route::get('/reports/download', [AMDFinancialReportController::class, 'download'])->name('reports.download');
+
+Route::get('/sre/preview', [StatementOfReceiptsAndExpidenturesController::class, 'preview'])->name('sre.preview');
+Route::get('/sre/download', [StatementOfReceiptsAndExpidenturesController::class, 'download'])->name('sre.download');
